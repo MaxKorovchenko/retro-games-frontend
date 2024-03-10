fbb0f10295bddca6f4063ba415e244fa37bee48b
MaxKorovchenko <max.korovchenko@gmail.com>
2024-03-10 16:52:11 +0200

    add default styles

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
-function App() {
-  return <div className="App">Retro Games</div>;
-}
-
-export default App;
+export const App = () => {
+  return (
+    <div className="App">
+      <h1>Retro games</h1>
+    </div>
+  );
+};
