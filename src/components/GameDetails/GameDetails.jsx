f019d216ecac3c55ceb0215ead05ce9fc13bc0f8
MaxKorovchenko <max.korovchenko@gmail.com>
2024-05-01 13:59:14 +0300

    update gameDetails and some styles

--- src/components/GameDetails/GameDetails.jsx
+++ src/components/GameDetails/GameDetails.jsx
@@ -1,17 +1,34 @@
-import { addToFavoriteGames } from 'myRedux/auth/operations';
-import { selectIsLoggedIn } from 'myRedux/auth/selectors';
-import { selectGames } from 'myRedux/games/selectors';
 import { useDispatch, useSelector } from 'react-redux';
 import { useParams } from 'react-router-dom';
+import { toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
+import { addToFavoriteGames } from 'myRedux/auth/operations';
+import {
+  selectIsLoading,
+  selectIsLoggedIn,
+  selectUser,
+} from 'myRedux/auth/selectors';
+import { selectGames } from 'myRedux/games/selectors';
 
 export const GameDetails = () => {
   const { gameId } = useParams();
   const games = useSelector(selectGames);
+  const { favoriteGames } = useSelector(selectUser);
   const isLoggedIn = useSelector(selectIsLoggedIn);
+  const isLoading = useSelector(selectIsLoading);
   const dispatch = useDispatch();
 
   const { title = '', platform = '' } =
     games.length && games.find(game => game._id === gameId);
+
+  const shouldDisableBtn =
+    !isLoggedIn || isLoading || favoriteGames.includes(gameId);
+
+  const handleAddToFavoriteGames = () =>
+    dispatch(addToFavoriteGames(gameId)).then(() =>
+      toast.success(`${title} successfully added to your favorites`)
+    );
 
   return (
     <div>
@@ -19,8 +36,8 @@ export const GameDetails = () => {
       <p>{platform}</p>
       <button
         type="button"
-        disabled={!isLoggedIn}
-        onClick={() => dispatch(addToFavoriteGames(gameId))}
+        disabled={shouldDisableBtn}
+        onClick={handleAddToFavoriteGames}
       >
         Add to favorite
       </button>
