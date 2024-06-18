b99156c5ec7e5181c3b68d9659e936642fa46e89
MaxKorovchenko <max.korovchenko@gmail.com>
2024-06-18 21:25:22 +0300

    update SleepingBackendMessage

--- src/components/SleepingBackendMessage/SleepingBackendMessage.jsx
+++ src/components/SleepingBackendMessage/SleepingBackendMessage.jsx
@@ -1,9 +1,40 @@
+import rr from 'assets/image/sleeping-back/rr.webp';
+import sonic from 'assets/image/sleeping-back/sonic.webp';
+import dd from 'assets/image/sleeping-back/darkwing-duck.webp';
+import mario from 'assets/image/sleeping-back/mario.webp';
+
 import styles from './SleepingBackendMessage.module.css';
+
+const images16 = [rr, sonic];
+const images8 = [dd, mario];
 
 export const SleepingBackendMessage = () => {
   return (
-    <h2 className={styles.message}>
-      The backend is currently sleeping. The application will start in 1 minute.
-    </h2>
+    <div className={styles.outerContainer}>
+      <div className={styles.container}>
+        {images16.map((image, index) => (
+          <img
+            key={index}
+            src={image}
+            alt={`cartoon ${index + 1}`}
+            className={`${styles.animatedImage16} ${styles[`delay${index}`]}`}
+          />
+        ))}
+
+        <p className={styles.message}>
+          The backend is currently sleeping. The application will start in 1
+          minute.
+        </p>
+
+        {images8.map((image, index) => (
+          <img
+            key={index}
+            src={image}
+            alt={`cartoon ${index + 1}`}
+            className={`${styles.animatedImage8} ${styles[`delay${index}`]}`}
+          />
+        ))}
+      </div>
+    </div>
   );
 };
