65a93b53dbeb6da397d79f3c94fcb593e376a650
MaxKorovchenko <max.korovchenko@gmail.com>
2024-08-20 12:52:46 +0300

    change register schema

--- src/helpers/validationSchemas/registerSchema.js
+++ src/helpers/validationSchemas/registerSchema.js
@@ -2,12 +2,8 @@ import * as Yup from 'yup';
 
 export const RegisterSchema = Yup.object().shape({
   name: Yup.string()
-    .min(2, 'Username must be at least 3 characters')
+    .min(2, 'Username must be at least 2 characters')
     .max(20, 'Username must be at most 20 characters')
-    .matches(
-      /^[a-zA-Z0-9_]+$/,
-      'Username can only contain letters, numbers, and underscores'
-    )
     .required('Username is required'),
   email: Yup.string()
     .email('Invalid email address')
