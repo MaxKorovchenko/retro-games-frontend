52dae4273cd997db5d21f1863221d6b6e77a71ba
MaxKorovchenko <max.korovchenko@gmail.com>
2024-11-30 11:20:26 +0200

    add background audio to SleepingBackendMessage component

--- src/components/SleepingBackendMessage/SleepingBackendMessage.jsx
+++ src/components/SleepingBackendMessage/SleepingBackendMessage.jsx
@@ -1,3 +1,5 @@
+import { useEffect, useRef, useState } from 'react';
+
 import rr from 'assets/image/sleeping-back/rr.webp';
 import sonic from 'assets/image/sleeping-back/sonic.webp';
 import jim from 'assets/image/sleeping-back/jim.webp';
@@ -7,6 +9,9 @@ import dd from 'assets/image/sleeping-back/darkwing-duck.webp';
 import mario from 'assets/image/sleeping-back/mario.webp';
 import chipDale from 'assets/image/sleeping-back/chip-dale.webp';
 import flinstones from 'assets/image/sleeping-back/flinstones.webp';
+import speakerOn from 'assets/image/svg/speaker-on-icon.svg';
+import speakerOff from 'assets/image/svg/speaker-off-icon.svg';
+import soundTrack from 'assets/audio/background-music.mp3';
 
 import styles from './SleepingBackendMessage.module.css';
 
@@ -14,9 +19,31 @@ const images16 = [rr, sonic, jim, cool];
 const images8 = [dd, mario, chipDale, flinstones];
 
 export const SleepingBackendMessage = () => {
+  const [isMuted, setIsMuted] = useState(true);
+  const audioRef = useRef(null);
+
+  useEffect(() => {
+    if (audioRef.current) {
+      isMuted ? audioRef.current.pause() : audioRef.current.play();
+    }
+  }, [isMuted]);
+
+  const toggleMute = () => {
+    setIsMuted(prevIsMuted => !prevIsMuted);
+  };
+
   return (
     <div className={styles.outerContainer}>
       <div className={styles.container}>
+        <audio ref={audioRef} src={soundTrack} loop />
+        <button onClick={toggleMute} className={styles.audioToggle}>
+          <img
+            src={isMuted ? speakerOff : speakerOn}
+            alt={isMuted ? 'Unmute Sound' : 'Mute Sound'}
+            className={styles.audioIcon}
+          />
+        </button>
+
         {images16.map((image, index) => (
           <img
             key={index}
