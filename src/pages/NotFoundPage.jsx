dd2cc865f8b54adb5488a9f959a2f946880ddaf7
MaxKorovchenko <max.korovchenko@gmail.com>
2025-09-25 17:49:40 +0300

    update not found page

--- src/pages/NotFoundPage.jsx
+++ src/pages/NotFoundPage.jsx
@@ -1,5 +1,37 @@
+const styles = {
+  wrapper: {
+    position: 'absolute',
+    top: '50%',
+    left: '50%',
+    transform: 'translate(-50%, -50%)',
+    textAlign: 'center',
+    color: '#dc181b',
+    letterSpacing: '20px',
+    textShadow: '1px 1px 2px yellow',
+    userSelect: 'none',
+  },
+  title: {
+    fontSize: 25,
+    marginBottom: 20,
+  },
+  subtitle: {
+    fontSize: 40,
+    marginBottom: 30,
+  },
+  text: {
+    fontSize: 50,
+    textShadow: '5px 5px 5px yellow',
+  },
+};
+
 const NotFoundPage = () => {
-  return <div>NotFoundPage</div>;
+  return (
+    <div style={styles.wrapper}>
+      <h1 style={styles.title}>PAGE NOT FOUND</h1>
+      <p style={styles.subtitle}>404</p>
+      <p style={styles.text}>GAME OVER</p>
+    </div>
+  );
 };
 
 export default NotFoundPage;
